8bf4e7e7a0d6609a46baa0dc0303293ff4fc89de
jpfMing21 <jpg@mingoville.com>
2013-09-15 13:31:21 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeFramed::Application.config.secret_key_base = 'fbe5da512c4ec03b81b28cc741e33f40b2262c4a16da1b106d295002410b9a315da295bec49a389bf1ba5cb145e160be974370a15e567aa648c581adbf440e7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimeFramed::Application.config.session_store :cookie_store, key: '_TimeFramed_session'
